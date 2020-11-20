e9cc480a0f7780150d9a0fceb9f6f0c3a719a22e
xo28122000 <jainam.2000@yahoo.com>
2020-11-20 13:56:04 -0500

    aded protected example routes in listing and inquiry routes

--- back_end/inquiryServer/index.js
+++ back_end/inquiryServer/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const passport = require("../lib/passport/index");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = 3002;
+
+app.use(cookieParser());
 
 app.post("/inquiry/sendMessage", (req, res) => {
   res.send("hello from /inquiry/sendMessage");
@@ -11,6 +15,13 @@ app.get("/inquiry/getUserInquiries", (req, res) => {
   res.send("hello from /inquiry/getUserInquiries");
 });
 
+app.post(
+  "/protected",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.send("reached the protected route " + req.user.name);
+  }
+);
 app.listen(port, () => {
   console.log(`server1 started at port ${port}`);
 });
--- back_end/listingServer/index.js
+++ back_end/listingServer/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const passport = require("../lib/passport/index");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = 3003;
+
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -33,6 +37,14 @@ app.get("/listing/getUserListings", (req, res) => {
 app.post("/listing/create", (req, res) => {
   res.send("hello from /listing/create");
 });
+
+app.post(
+  "/protected",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.send("reached the protected route " + req.user.name);
+  }
+);
 
 app.listen(port, () => {
   console.log(`server1 started at port ${port}`);
